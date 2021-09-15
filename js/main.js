8cc9dc52e7ff551509b054d0d5048f85eefe3e42
bgrim <bgrimshaw37@gmail.com>
2021-09-14 19:07:51 -0600

    check for week

--- js/main.js
+++ js/main.js
@@ -5,6 +5,6 @@ const links = ["Week1 notes", "Week2 Notes"]
 for(var i=0; i<links.length; i++) {
 
     document.getElementById("my-list").innerHTML+=
-        '<li class="list"><a href="week' + (i+1) + 'index.html">' + links[i] +  '</a> </li>';
+        '<li class="list"><a href="week'  + 'index.html">' + links[i] +  '</a> </li>';
 
     }
