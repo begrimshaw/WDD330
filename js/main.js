8756ab1cbcb45e88fdf46cc71b5c863d3ef9a8af
bgrim <bgrimshaw37@gmail.com>
2021-09-14 21:52:21 -0600

    trying to add week 1 link

--- js/main.js
+++ js/main.js
@@ -4,7 +4,8 @@ const links = ["Week 1 Notes", "Week2 Notes"]
 
 for(var i=0; i<links.length; i++) {
 
+    j = i + 1; 
     document.getElementById("my-list").innerHTML+=
-        '<li class="list"><a href="week'  + 'index.html">' + links[i] +  '</a></li>';
+        '<li class="list"><a href="week' + j  + '/">' + links[i] +  '</a></li>';
 
     }
