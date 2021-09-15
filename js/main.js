a44197395fd1b7a29c577f59e4a3effd4ae311ff
bgrim <bgrimshaw37@gmail.com>
2021-09-15 10:12:21 -0600

    week 1 notes and files

--- js/main.js
+++ js/main.js
@@ -7,5 +7,4 @@ for(var i=0; i<links.length; i++) {
     j = i + 1; 
     document.getElementById("my-list").innerHTML+=
         '<li class="list"><a href="week' + j  + '/">' + links[i] +  '</a></li>';
-
     }
