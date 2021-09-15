1f2b878a3c94a3e0f2f0338d768e75739ee37b36
bgrim <bgrimshaw37@gmail.com>
2021-09-14 19:10:26 -0600

    check for pring

--- js/main.js
+++ js/main.js
@@ -5,6 +5,6 @@ const links = ["Week1 notes", "Week2 Notes"]
 for(var i=0; i<links.length; i++) {
 
     document.getElementById("my-list").innerHTML+=
-        '<li class="list"><a href="week'  + 'index.html">' + links[i] +  '</a> </li>';
+        '<li class="list">checking<a href="week'  + 'index.html">' + links[i] +  '</a></li>';
 
     }
